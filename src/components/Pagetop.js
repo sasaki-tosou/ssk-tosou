f211802cfe164db02ea129233b4dfd6e972c1918
blancTo <git@blanc.to>
2023-02-25 07:22:48 +0900

    main

--- src/components/Pagetop.js
+++ src/components/Pagetop.js
@@ -16,8 +16,8 @@ const Pagetop = () => {
                 <span>ライン見積もり</span>
             </a>
             </li>
-            <li className="tub"><a href="https://twitter.com/sasakipaint1116" aria-label="佐々木塗装のTwitter" target="_blank" rel="noopenner noreferrer" className="tw_bt"><img src="../images/tw_icon.png" width={25} height={25} className="opa" alt="" /></a></li>
-            <li className="tub"><a href="https://www.facebook.com/profile.php?id=100063604571277" aria-label="佐々木塗装のFacebook" target="_blank" rel="noopenner noreferrer" className="fb_bt"><img src="../images/fb_icon.png" width={25} height={25} className="opa" alt="" /></a></li>
+            <li className="tub"><a href="https://twitter.com/sasakipaint1116" aria-label="佐々木塗装のTwitter" target="_blank" rel="noopenner noreferrer" className="tw_bt"><img src="/images/tw_icon.png" width={25} height={25} className="opa" alt="" /></a></li>
+            <li className="tub"><a href="https://www.facebook.com/profile.php?id=100063604571277" aria-label="佐々木塗装のFacebook" target="_blank" rel="noopenner noreferrer" className="fb_bt"><img src="/images/fb_icon.png" width={25} height={25} className="opa" alt="" /></a></li>
             <li className="tub"><a href="https://www.instagram.com/sasaki.ryoko/" aria-label="佐々木塗装のinstagram" target="_blank" rel="noopenner noreferrer" className="insta_bt"><span className="insta"><img src="/images/insta_icon.svg" width={25} height={25} className="opa" alt="" /></span></a></li>
         </ul>
     </>
