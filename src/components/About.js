c88c24683a14d9c4b10dcc67ee0bd71b3724bd7b
blancTo <git@blanc.to>
2023-02-25 07:03:24 +0900

    main

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ const about = () => {
                         <p><Link to="/company/" className="bt01"><FontAwesomeIcon icon={faAngleRight} /> 会社概要はこちら</Link></p>
                     </div>
                     <div className="about_r">
-                    <StaticImage src="/images/about_pic01.jpg" width={761} height={508} loading="lazy" alt="" />
+                        <StaticImage src="../images/about_pic01.jpg" width={761} height={508} loading="lazy" alt="" />
                     </div>
                 </div>
             </div>
@@ -44,7 +44,7 @@ const about = () => {
                     </div>
                     
                     <div className="point_r">
-                    <StaticImage src="/images/point_pic01.jpg" width={761} height={508} loading="lazy" alt="" />
+                        <StaticImage src="../images/point_pic01.jpg" width={761} height={508} loading="lazy" alt="" />
                     </div>
                 </div>
             </div>
