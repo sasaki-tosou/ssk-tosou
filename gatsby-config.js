2bc2c14eabad351c38ad6a16a2487e2446339227
blancTo <git@blanc.to>
2023-09-29 03:29:34 +0900

    Prettierのコード整形

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,20 +14,22 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "広島で外壁塗装・屋根塗装なら佐々木塗装。一級塗装技能士在籍、塗装のプロフェッショナル集団。",
-    keyword:"佐々木塗装,外壁塗装,広島,外壁塗装 広島,広島 外壁塗装,屋根塗装,塗り替え,一級塗装技能士在籍,安佐北区,東区,西区,南区,安芸区,佐伯区,安佐南区,海田町,府中町,熊野町,坂町,五日市,廿日市市,東広島市,安芸高田市",
-    description: "安佐北区の施工実績No.1。広島で外壁塗装・屋根塗装なら、広島初のドローンを使用した外装劣化診断士による診断。安心施工、保証付きのアフターサービス。大切な住まいの塗り替えは一級塗装技能士在籍、塗装のプロフェッショナル集団。屋根・外壁塗装の専門店の佐々木塗装にお任せください。",
+    title:
+      "広島で外壁塗装・屋根塗装なら佐々木塗装。一級塗装技能士在籍、塗装のプロフェッショナル集団。",
+    keyword:
+      "佐々木塗装,外壁塗装,広島,外壁塗装 広島,広島 外壁塗装,屋根塗装,塗り替え,一級塗装技能士在籍,安佐北区,東区,西区,南区,安芸区,佐伯区,安佐南区,海田町,府中町,熊野町,坂町,五日市,廿日市市,東広島市,安芸高田市",
+    description:
+      "安佐北区の施工実績No.1。広島で外壁塗装・屋根塗装なら、広島初のドローンを使用した外装劣化診断士による診断。安心施工、保証付きのアフターサービス。大切な住まいの塗り替えは一級塗装技能士在籍、塗装のプロフェッショナル集団。屋根・外壁塗装の専門店の佐々木塗装にお任せください。",
     image: "/images/mobile_img.jpg",
     url: "https://sasaki-tosou.co.jp",
     siteUrl: "https://sasaki-tosou.co.jp/",
     twitterUsername: "sasakipaint1116",
   },
   plugins: [
-
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
-    
+
     {
       resolve: "gatsby-plugin-google-tagmanager",
       options: {
@@ -50,16 +52,16 @@ module.exports = {
 
     {
       resolve: `gatsby-transformer-remark`,
-      options: {        
+      options: {
         plugins: [`gatsby-remark-responsive-iframe`],
       },
     },
-    
+
     {
       resolve: "gatsby-plugin-anchor-links",
       options: {
-        offset: -100
-      }
+        offset: -100,
+      },
     },
 
     {
@@ -79,16 +81,16 @@ module.exports = {
     },
 
     {
-      resolve: 'gatsby-source-microcms',
+      resolve: "gatsby-source-microcms",
       options: {
         apiKey: process.env.API_KEY,
-        serviceId: 'sasaki-tosou',
+        serviceId: "sasaki-tosou",
         apis: [
           {
-            endpoint: 'case',
+            endpoint: "case",
           },
           {
-            endpoint: 'category',
+            endpoint: "category",
           },
           {
             endpoint: "blog",
@@ -96,8 +98,6 @@ module.exports = {
         ],
       },
     },
-    
-    
 
     {
       resolve: `gatsby-plugin-canonical-urls`,
@@ -106,15 +106,13 @@ module.exports = {
       },
     },
 
-    
-
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-robots-txt`,
       options: {
         host: `https://sasaki-tosou.co.jp`,
         sitemap: `https://sasaki-tosou.co.jp/sitemap-0.xml`,
-        policy: [{userAgent: '*', allow: '/'}]
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
 
@@ -134,6 +132,5 @@ module.exports = {
         },
       },
     },
-    
   ],
-}
+};
