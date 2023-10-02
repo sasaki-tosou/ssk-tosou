46879710e401a06e89ecde727d0c976a863b8558
blancTo <git@blanc.to>
2023-10-03 05:51:51 +0900

    ブログ記事一覧ページ作成

--- src/templates/all-posts.js
+++ src/templates/all-posts.js
@@ -2,6 +2,7 @@ import React from "react";
 import { graphql, Link } from "gatsby";
 import Seo from "../components/Seo";
 import Layout from "../components/Layout";
+import Sideb from "../components/Sideb";
 
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
@@ -12,8 +13,8 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault("Asia/Tokyo");
 
-const MAX_CONTENT_LENGTH = 100; // 最大文字数を設定してください
-const postsPerPage = 10; // 1ページあたりの記事数
+const MAX_CONTENT_LENGTH = 50; // 最大文字数を設定してください
+const postsPerPage = 30; // 1ページあたりの記事数
 const pagePadding = 1; // 前後に追加で表示するページ数
 
 const AllPosts = ({ data, pageContext }) => {
@@ -21,114 +22,160 @@ const AllPosts = ({ data, pageContext }) => {
   const { allMicrocmsBlog } = data;
   const { numPages, currentPage, startPage, endPage } = pageContext;
   const posts = allMicrocmsBlog.edges;
-  const postsPerPage = pageContext.postsPerPage || 10;
-
+  const postsPerPage = pageContext.postsPerPage || 30;
+  const stripHTML = (html) => {
+    const tmp = document.createElement("div");
+    tmp.innerHTML = html;
+    return tmp.textContent || tmp.innerText || "";
+  };
   return (
     <Layout>
-      <div id="mainimage-sub" className="yane-cover">
+      <div id="mainimage-sub" className="blog">
         <div id="mainimage-inner"></div>
       </div>
-
+      <div id="breadcrumb">
+        <ul
+          className="breadcrumb__list"
+          itemscope
+          itemtype="https://schema.org/BreadcrumbList"
+        >
+          <li
+            className="breadcrumb__item"
+            itemprop="itemListElement"
+            itemscope
+            itemtype="https://schema.org/ListItem"
+          >
+            <Link to="/" itemprop="item">
+              <span itemprop="name">ホーム</span>
+            </Link>
+            <meta itemprop="position" content="1" />
+          </li>
+          <li
+            className="breadcrumb__item"
+            itemprop="itemListElement"
+            itemscope
+            itemtype="https://schema.org/ListItem"
+          >
+            <Link to="/blog/" itemprop="item">
+              <span itemprop="name">ブログ</span>
+            </Link>
+            <meta itemprop="position" content="2" />
+          </li>
+        </ul>
+      </div>
       <section id="sub-page">
         <div className="main-content">
-          <h3 className="main_title">ブログ</h3>
+          <div className="main_b">
+            <h2 class="page_title01 mt0">ブログ</h2>
 
-          {posts.map(({ node }) => (
-            <div className="column_box" key={node.blogId}>
-              {node.mainimage ? (
-                <div className="column_img">
-                  <a href={"/posts/" + node.blogId + "/"}>
-                    <img
-                      src={node.mainimage.url + "?fm=webp"}
-                      width={370}
-                      height={277}
-                      alt={node.title + "サムネイル画像"}
-                      loading="lazy"
-                    />
-                  </a>
-                </div>
-              ) : (
-                <div className="column_img">
-                  <a href={"/posts/" + node.blogId + "/"}>
-                    <img
-                      src="/img/alternative-image.png"
-                      width={370}
-                      height={277}
-                      alt={node.title + "の代替画像"}
-                      loading="lazy"
-                    />
-                  </a>
-                </div>
-              )}
-              <div className="column_txt">
-                <time
-                  dateTime={dayjs
-                    .utc(node.date)
-                    .tz("Asia/Tokyo")
-                    .format("YYYY-MM-DDTHH:mm:ss")}
-                >
-                  {dayjs.utc(node.date).tz("Asia/Tokyo").format("YYYY/MM/DD")}
-                </time>
-                <a href={"/posts/" + node.blogId + "/"}>{node.title}</a>
-                {stripHTML(node.content).length > MAX_CONTENT_LENGTH
-                  ? stripHTML(node.content).substring(0, MAX_CONTENT_LENGTH) +
-                    "..."
-                  : stripHTML(node.content)}
-              </div>
-            </div>
-          ))}
-
-          {/* ページネーションリンク */}
-          {numPages > 1 && (
-            <div className="pager">
-              {currentPage > 1 && (
-                <Link
-                  to={currentPage === 2 ? `/blog` : `/blog/${currentPage - 1}`}
-                >
-                  &lt;&lt; 前へ
-                </Link>
-              )}
-
-              {/* ページャーの数字を表示 */}
-              {Array.from({ length: numPages }, (_, i) => {
-                const pageNumber = startPage + i; // ページャーの数字を計算
-                if (pageNumber <= endPage) {
-                  return (
-                    <Link
-                      key={`pagination-link${pageNumber}`}
-                      to={pageNumber === 1 ? `/blog` : `/blog/${pageNumber}`}
-                      className={pageNumber === currentPage ? "current" : ""}
+            <div className="kiji_list">
+              {posts.map(({ node }) => (
+                <div className="kiji_box" key={node.blogId}>
+                  {node.mainimage ? (
+                    <div className="kiji_thumb">
+                      <a href={"/posts/" + node.blogId + "/"}>
+                        <img
+                          src={node.mainimage.url + "?fm=webp"}
+                          width={370}
+                          height={277}
+                          alt={node.title + "サムネイル画像"}
+                          loading="lazy"
+                        />
+                      </a>
+                    </div>
+                  ) : (
+                    <div className="kiji_thumb">
+                      <a href={"/posts/" + node.blogId + "/"}>
+                        <img
+                          src="/img/alternative-image.png"
+                          width={370}
+                          height={277}
+                          alt={node.title + "の代替画像"}
+                          loading="lazy"
+                        />
+                      </a>
+                    </div>
+                  )}
+                  <div className="kiji_txt">
+                    <time
+                      dateTime={dayjs
+                        .utc(node.date)
+                        .tz("Asia/Tokyo")
+                        .format("YYYY-MM-DDTHH:mm:ss")}
                     >
-                      {pageNumber}
-                    </Link>
-                  );
-                }
-                return null;
-              })}
+                      {dayjs
+                        .utc(node.date)
+                        .tz("Asia/Tokyo")
+                        .format("YYYY/MM/DD")}
+                    </time>
+                    <a href={"/posts/" + node.blogId + "/"}>{node.title}</a>
 
-              {currentPage < numPages && (
-                <Link to={`/blog/${currentPage + 1}`}>次へ &gt;&gt;</Link>
-              )}
+                    {stripHTML(node.body).length > MAX_CONTENT_LENGTH
+                      ? stripHTML(node.body).substring(0, MAX_CONTENT_LENGTH) +
+                        "..."
+                      : stripHTML(node.body)}
+
+                    <ul className="cat_list">
+                      <li className={`${node.category.id}`}>
+                        <Link to={`/category/${node.category.id}`}>
+                          {node.category.name}
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              ))}
             </div>
-          )}
+
+            {/* ページネーションリンク */}
+            {numPages > 1 && (
+              <div className="pager">
+                {currentPage > 1 && (
+                  <Link
+                    to={
+                      currentPage === 2 ? `/blog/` : `/blog/${currentPage - 1}`
+                    }
+                  >
+                    &lt;&lt; 前へ
+                  </Link>
+                )}
+
+                {/* ページャーの数字を表示 */}
+                {Array.from({ length: numPages }, (_, i) => {
+                  const pageNumber = startPage + i; // ページャーの数字を計算
+                  if (pageNumber <= endPage) {
+                    return (
+                      <Link
+                        key={`pagination-link${pageNumber}`}
+                        to={pageNumber === 1 ? `/blog/` : `/blog/${pageNumber}`}
+                        className={pageNumber === currentPage ? "current" : ""}
+                      >
+                        {pageNumber}
+                      </Link>
+                    );
+                  }
+                  return null;
+                })}
+
+                {currentPage < numPages && (
+                  <Link to={`/blog/${currentPage + 1}`}>次へ &gt;&gt;</Link>
+                )}
+              </div>
+            )}
+          </div>
+          <aside className="side_b">
+            <Sideb />
+          </aside>
         </div>
       </section>
     </Layout>
   );
 };
 
-// HTMLタグを削除する関数
-function stripHTML(html) {
-  if (html === undefined || html === null) {
-    return ""; // もし html が undefined または null の場合、空文字列を返す
-  }
-  return html.replace(/<[^>]*>/g, "");
-}
-
-export const Head = ({ data }) => {
+export const Head = () => {
   return (
     <>
-      <body id="pagetop" />
+      <body id="pagetop" className="blogpage" />
       <Seo title="ブログ記事一覧" />
     </>
   );
@@ -153,6 +200,10 @@ export const query = graphql`
             url
           }
           body
+          category {
+            name
+            id
+          }
         }
       }
     }
