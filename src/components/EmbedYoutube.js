1097166bf6e6f28a2801257b3b13d0a48d095c37
blancTo <git@blanc.to>
2023-10-04 04:15:37 +0900

    不要なもの削除、パッケージ依存関係再構築

--- src/components/EmbedYoutube.js
+++ src/components/EmbedYoutube.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import * as styles from '../css/embedyoutube.module.scss';
+import React, { useState } from "react";
+import * as styles from "../css/embedyoutube.module.scss";
 
 const EmbedYoutube = ({ id }) => {
   const [isThumbnail, setIsThumbnail] = useState(true);
@@ -9,7 +9,7 @@ const EmbedYoutube = ({ id }) => {
   };
 
   const handleKeyDown = (e) => {
-    if (e.key === 'Enter' || e.key === 'Space') {
+    if (e.key === "Enter" || e.key === "Space") {
       setIsThumbnail(false);
     }
   };
