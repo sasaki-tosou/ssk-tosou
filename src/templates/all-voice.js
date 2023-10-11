91bfefd16068ac19b9e6f2785a952de827ba8723
blancTo <git@blanc.to>
2023-10-12 05:05:20 +0900

    お客様の声を投稿できるように調整

---
+++ src/templates/all-voice.js
@@ -0,0 +1,218 @@
+import React from "react";
+import { graphql, Link } from "gatsby";
+import Seo from "../components/Seo";
+import Layout from "../components/Layout";
+import Sideb from "../components/Sideb";
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Tokyo");
+
+const postsPerPage = 10; // 1ページあたりの記事数
+const pagePadding = 1; // 前後に追加で表示するページ数
+
+const AllVoice = ({ data, pageContext }) => {
+  dayjs.locale("ja");
+  const { allMicrocmsCase } = data;
+  const { numPages, currentPage, startPage, endPage } = pageContext;
+  const posts = allMicrocmsCase.edges;
+  const postsPerPage = pageContext.postsPerPage || 10;
+
+  return (
+    <Layout>
+      <div id="mainimage-sub" className="case">
+        <div id="mainimage-inner"></div>
+      </div>
+      <div id="breadcrumb">
+        <ul
+          className="breadcrumb__list"
+          itemscope
+          itemtype="https://schema.org/BreadcrumbList"
+        >
+          <li
+            className="breadcrumb__item"
+            itemprop="itemListElement"
+            itemscope
+            itemtype="https://schema.org/ListItem"
+          >
+            <Link to="/" itemprop="item">
+              <span itemprop="name">ホーム</span>
+            </Link>
+            <meta itemprop="position" content="1" />
+          </li>
+          <li
+            className="breadcrumb__item"
+            itemprop="itemListElement"
+            itemscope
+            itemtype="https://schema.org/ListItem"
+          >
+            <Link to="/voice_new/" itemprop="item">
+              <span itemprop="name">お客様の声</span>
+            </Link>
+            <meta itemprop="position" content="2" />
+          </li>
+        </ul>
+      </div>
+
+      <section id="sub-page">
+        <div className="main-content">
+          <div className="main_b">
+            <h2 class="page_title01 mt0">お客様の声</h2>
+
+            {posts.map(({ node }) => (
+              <div className="user_voice_box">
+                <h3 className="voice_title">{node.title}</h3>
+                <p className="date">
+                  更新日：
+                  <time
+                    dateTime={dayjs
+                      .utc(node.date)
+                      .tz("Asia/Tokyo")
+                      .format("YYYY-MM-DDTHH:mm:ss")}
+                  >
+                    {dayjs.utc(node.date).tz("Asia/Tokyo").format("YYYY/MM/DD")}
+                  </time>
+                </p>
+                <div className="flex-wrap">
+                  <div className="kansya_img">
+                    {node.maeImg && (
+                      <img
+                        src={node.maeImg.url + "?fm=webp"}
+                        alt={node.title + "サムネイル画像"}
+                        loading="lazy"
+                      />
+                    )}
+                  </div>
+                  <div className="kansya_img">
+                    {node.atoImg && (
+                      <img
+                        src={node.atoImg.url + "?fm=webp"}
+                        alt={node.title + "サムネイル画像"}
+                        loading="lazy"
+                      />
+                    )}
+                  </div>
+                </div>
+
+                {node.casetxt && (
+                  <>
+                    <h4 className="voice_subtitle">お客様からのコメント</h4>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: `${node.casetxt}`,
+                      }}
+                    />
+                  </>
+                )}
+
+                {node.caseDescription && (
+                  <>
+                    <h4 className="voice_subtitle">オススメポイント</h4>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: `${node.caseDescription}`,
+                      }}
+                    />
+                  </>
+                )}
+              </div>
+            ))}
+
+            {numPages > 1 && posts.length > 1 && (
+              <div className="pager">
+                {currentPage > 1 && (
+                  <Link
+                    to={
+                      currentPage === 2
+                        ? `/voice_new`
+                        : `/voice_new/${currentPage - 1}`
+                    }
+                  >
+                    &lt;&lt; 前へ
+                  </Link>
+                )}
+                {/* ページャーの数字を表示 */}
+                {Array.from({ length: numPages }, (_, i) => {
+                  const pageNumber = startPage + i; // ページャーの数字を計算
+                  if (pageNumber <= endPage) {
+                    return (
+                      <Link
+                        key={`pagination-link${pageNumber}`}
+                        to={
+                          pageNumber === 1
+                            ? `/voice_new`
+                            : `/voice_new/${pageNumber}`
+                        }
+                        className={pageNumber === currentPage ? "current" : ""}
+                      >
+                        {pageNumber}
+                      </Link>
+                    );
+                  }
+                  return null;
+                })}
+                {currentPage < numPages && (
+                  <Link to={`/voice_new/${currentPage + 1}`}>
+                    次へ &gt;&gt;
+                  </Link>
+                )}
+              </div>
+            )}
+          </div>
+          <aside className="side_b">
+            <Sideb />
+          </aside>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export const Head = ({ data }) => {
+  return (
+    <>
+      <body id="pagetop" className="blogpage" />
+      <Seo
+        title2={`お客様の声一覧｜外壁塗装なら広島の佐々木塗装`}
+        description={`外壁塗装なら広島の佐々木塗装｜お客様の声一覧`}
+      />
+    </>
+  );
+};
+
+export const query = graphql`
+  query ($limit: Int, $skip: Int) {
+    allMicrocmsCase(
+      limit: $limit
+      skip: $skip
+      sort: { date: DESC }
+      filter: { category: { id: { in: "voice" } } }
+    ) {
+      edges {
+        node {
+          caseId
+          title
+          date
+          eyecatch {
+            url
+          }
+          casetxt
+          maeImg {
+            url
+          }
+          atoImg {
+            url
+          }
+          caseDescription
+          casePeriod
+        }
+      }
+    }
+  }
+`;
+
+export default AllVoice;
---
+++ src/templates/voice.js
@@ -0,0 +1,155 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Seo from "../components/Seo";
+import Layout from "../components/Layout";
+import Sideb from "../components/Sideb";
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Tokyo");
+
+const VoicePage = ({ data }) => {
+  dayjs.locale("ja");
+
+  return (
+    <>
+      <Layout>
+        <div id="mainimage-sub" className="case">
+          <div id="mainimage-inner"></div>
+        </div>
+
+        <section id="sub-page">
+          <div className="main-content">
+            <article className="main_b">
+              <h2 className="page_title01 mt0">施工事例</h2>
+
+              <div className="user_voice_box">
+                <h3 className="voice_title">{data.microcmsCase.title}</h3>
+                <p className="date">
+                  更新日：
+                  <time
+                    dateTime={dayjs
+                      .utc(data.microcmsCase.date)
+                      .tz("Asia/Tokyo")
+                      .format("YYYY-MM-DDTHH:mm:ss")}
+                  >
+                    {dayjs
+                      .utc(data.microcmsCase.date)
+                      .tz("Asia/Tokyo")
+                      .format("YYYY/MM/DD")}
+                  </time>
+                </p>
+                <div className="flex-wrap">
+                  <div className="kansya_img">
+                    {data.microcmsCase.maeImg && (
+                      <img
+                        src={data.microcmsCase.maeImg.url + "?fm=webp"}
+                        alt={data.microcmsCase.title + "サムネイル画像"}
+                        loading="lazy"
+                      />
+                    )}
+                  </div>
+                  <div className="kansya_img">
+                    {data.microcmsCase.atoImg && (
+                      <img
+                        src={data.microcmsCase.atoImg.url + "?fm=webp"}
+                        alt={data.microcmsCase.title + "サムネイル画像"}
+                        loading="lazy"
+                      />
+                    )}
+                  </div>
+                </div>
+
+                {data.microcmsCase.casetxt && (
+                  <>
+                    <h4 className="voice_subtitle">お客様からのコメント</h4>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: `${data.microcmsCase.casetxt}`,
+                      }}
+                    />
+                  </>
+                )}
+
+                {data.microcmsCase.caseDescription && (
+                  <>
+                    <h4 className="voice_subtitle">オススメポイント</h4>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: `${data.microcmsCase.caseDescription}`,
+                      }}
+                    />
+                  </>
+                )}
+              </div>
+
+              <p class="center">
+                <a href={`/voice_new/`} className="bt02">
+                  一覧に戻る
+                </a>
+              </p>
+            </article>
+            <aside className="side_b">
+              <Sideb />
+            </aside>
+          </div>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export const Head = ({ data }) => {
+  const pageName = data.microcmsCase.title;
+  const maxTextLength = 80;
+
+  const body = data.microcmsCase.casetxt;
+  const truncatedBody = body.replace(/<[^>]+>/g, "");
+  const limitedBody =
+    truncatedBody.length > maxTextLength
+      ? truncatedBody.slice(0, maxTextLength) + "..."
+      : truncatedBody;
+
+  return (
+    <>
+      <body id="pagetop" className="blogpage" />
+      <Seo
+        title2={`${pageName}｜外壁塗装なら広島の佐々木塗装`}
+        description={limitedBody}
+      />
+    </>
+  );
+};
+
+export const query = graphql`
+  query ($id: String) {
+    microcmsCase(id: { eq: $id }, category: { id: { in: "voice" } }) {
+      title
+      category {
+        id
+        name
+      }
+      caseId
+      date
+      id
+      casetxt
+      eyecatch {
+        url
+      }
+      maeImg {
+        url
+      }
+      atoImg {
+        url
+      }
+      casePeriod
+      caseDescription
+    }
+  }
+`;
+
+export default VoicePage;
