28570239ee594b7a8a21af6d517774b3b86c133a
blancTo <git@blanc.to>
2024-02-07 07:33:44 +0900

    DocsBot表示追加

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,7 +5,33 @@
  */
 
 // You can delete this file if you're not using it
+const React = require('react');
 
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-    setHtmlAttributes({ lang: "ja" })
-}
+exports.onRenderBody = ({ setHtmlAttributes, setPostBodyComponents }) => {
+    // HTML属性を設定
+    setHtmlAttributes({ lang: "ja" });
+  
+    // bodyタグの終了直前に追加するスクリプト
+    setPostBodyComponents([
+      
+        <script
+        key="docsbotai-script"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.DocsBotAI=window.DocsBotAI||{},DocsBotAI.init=function(c){return new Promise(function(e,o){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://widget.docsbot.ai/chat.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n),t.addEventListener("load",function(){window.DocsBotAI.mount({id:c.id,supportCallback:c.supportCallback,identify:c.identify,options:c.options,signature:c.signature});var t;t=function(n){return new Promise(function(e){if(document.querySelector(n))return e(document.querySelector(n));var o=new MutationObserver(function(t){document.querySelector(n)&&(e(document.querySelector(n)),o.disconnect())});o.observe(document.body,{childList:!0,subtree:!0})})},t&&t("#docsbotai-root").then(e).catch(o)}),t.addEventListener("error",function(t){o(t.message)})})};
+          `
+        }}
+      />,
+      <script
+        key="docsbotai-init"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            DocsBotAI.init({id: "2Uhgb5nD9Pb523gkIyc1/MFW6KuiFdcX37EIVBTTP"});
+          `
+        }}
+      />
+      
+    ]);
+  };
